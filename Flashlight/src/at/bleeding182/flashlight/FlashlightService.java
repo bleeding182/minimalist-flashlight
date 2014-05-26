cab3f01bcbb496b9ac7533a7a6b14e15d6a5ab8d
bleeding182 <davidmedenjak@gmail.com>
2014-05-26 17:28:41 +0200

    Added support for more devices by adding surface view and added some error handling in case no flashmodes are supported

--- Flashlight/src/at/bleeding182/flashlight/FlashlightService.java
+++ Flashlight/src/at/bleeding182/flashlight/FlashlightService.java
@@ -1,7 +1,12 @@
 package at.bleeding182.flashlight;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.IBinder;
@@ -28,15 +33,31 @@ public class FlashlightService extends Service {
 		try {
 			cam = Camera.open();
 			Parameters p = cam.getParameters();
-			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			List<String> flashes = p.getSupportedFlashModes();
+			if (flashes == null)
+				return error(this, "Flash not available.");
+			if (flashes.contains(Parameters.FLASH_MODE_TORCH))
+				p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			else if (flashes.contains(Parameters.FLASH_MODE_ON))
+				p.setFlashMode(Parameters.FLASH_MODE_ON);
+			else
+				return error(this, "Flash not available.");
 			cam.setParameters(p);
-			cam.startPreview(); // Not needed for all devices it seems.
+			// Not needed for all devices.
+			cam.setPreviewTexture(new SurfaceTexture(0));
+			cam.startPreview();
+			return START_NOT_STICKY;
 		} catch (RuntimeException e) {
-			Toast.makeText(getApplicationContext(), "Could not access camera.",
-					Toast.LENGTH_SHORT).show();
-			sendBroadcast(new Intent(getApplication(), FlashlightProvider.class)
-					.setAction(FlashlightProvider.TOGGLE_ACTION));
+			return error(this, "Could not access camera.");
+		} catch (IOException e) {
+			return error(this, "Could not activate camera.");
 		}
+	}
+
+	private static int error(Context context, String toast) {
+		Toast.makeText(context, toast, Toast.LENGTH_SHORT).show();
+		context.sendBroadcast(new Intent(context, FlashlightProvider.class)
+				.setAction(FlashlightProvider.TOGGLE_ACTION));
 		return START_NOT_STICKY;
 	}
 
